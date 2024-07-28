f8ba5ac3d55d38c9cc66f7b29c36477e9a6e30fa
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-07-28 20:44:34 -0300

    Css Global

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header/Header";
 
+import './global.css'
+
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello, world!</h1>
+      <h1>Hello World!</h1>
     </>
   );
 }
