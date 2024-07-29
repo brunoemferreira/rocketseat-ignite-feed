0a18794ce321b64de7383cec879cd93f72c21cc0
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-07-28 22:08:18 -0300

    "Added Sidebar component and updated App layout with wrapper, aside, and main elements"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
-import './global.css'
+import styles from "./App.module.css";
+
+import "./global.css";
+
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello World!</h1>
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+        <main>
+          <h1>Hello World!</h1>
+        </main>
+      </div>
     </>
   );
 }
