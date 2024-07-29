5978565d5ba67b791bf0441e64158a1167d93f95
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-07-29 00:11:49 -0300

    "Added Post component and removed Hello World heading, updated App component structure"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { Header } from "./components/Header/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Post } from "./components/Post/Post";
 
 import styles from "./App.module.css";
-
 import "./global.css";
-
 
 function App() {
   return (
@@ -13,10 +12,17 @@ function App() {
 
       <div className={styles.wrapper}>
         <aside>
-          <Sidebar/>
+          <Sidebar />
         </aside>
         <main>
-          <h1>Hello World!</h1>
+          <Post
+            author="Bruno Eduardo de Moura Ferreira"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          />
+          <Post
+            author="Juvenal da Silva"
+            content="Lorem Ipsum is typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          />
         </main>
       </div>
     </>
