4a75eeffb5e37f142050b799c0bc356445da8ee4
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-07-28 23:43:30 -0300

    "Added @phosphor-icons/react dependency and integrated PencilLine icon into Sidebar component"

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "@phosphor-icons/react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,12 +10,20 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/brunoemferreira.png"
+        />
+
         <strong>Bruno Eduardo de Moura Ferreira</strong>
         <span>FullStack Developer</span>
       </div>
 
       <footer>
-        <a href="#"> Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
